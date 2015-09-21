139abf245b075915789b4e18b4e89dc548190655
Sudhir Kumar <sudhir1.kumar@citi.com>
2015-09-21 15:59:51 -0500

    Initial Commit

---
+++ Local_NotificationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Local_NotificationExample
+//
+//  Created by Sudhir Kumar on 9/21/15.
+//  Copyright (c) 2015 Citibank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Local_NotificationExampleTests/Local_NotificationExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Local_NotificationExampleTests.swift
+//  Local_NotificationExampleTests
+//
+//  Created by Sudhir Kumar on 9/21/15.
+//  Copyright (c) 2015 Citibank. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Local_NotificationExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
